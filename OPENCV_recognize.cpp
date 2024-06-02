8308e5cd8d62d611813dafe6fa2d050cb53c34b7
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2024-06-02 22:04:15 +0900

    Update OPENCV_recognize.cpp

--- OPENCV_recognize.cpp
+++ OPENCV_recognize.cpp
@@ -6,9 +6,20 @@ using namespace cv;
 using namespace cv::dnn;
 using namespace std;
 
+Mat save_black = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Save_black.jpg");
+Mat save_red = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Save_red.jpg");
+Mat load_black = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Load_black.jpg");
+Mat load_red = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Load_red.jpg");
+Mat clear_black = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Clear_black.jpg");
+Mat clear_red = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Clear_red.jpg");
+Mat run_black = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Run_black.jpg");
+Mat run_red = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Run_red.jpg");
+Mat exit_black = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Exit_black.jpg");
+Mat exit_red = imread("C:/Users/AIRLAB/source/repos/OpenCV/OpenCV/Exit_red.jpg");
 
+void copyimg(Mat& img, Rect area, Mat& copy);
 Mat drawLineImg();
-void wrigt_number(Mat board);
+void turn_menu_color(Mat board);
 void on_mouse(int event, int x, int y, int flags, void* userdata);
 
 int main(void)
@@ -22,6 +33,14 @@ int main(void)
     return 0;
 }
 
+void copyimg(Mat& img, Rect area, Mat& copy) {
+    resize(copy, copy, Size(120, 70));
+    Point start(area.x, area.y);
+    start.x += (area.width - copy.cols) / 2;
+    start.y += (area.height - copy.rows) / 2;
+
+    copy.copyTo(img(Rect(start.x, start.y, copy.cols, copy.rows)));
+}
 
 Mat drawLineImg() {
     Mat board(500, 650, CV_8UC3, Scalar(255, 255, 255));
@@ -31,56 +50,48 @@ Mat drawLineImg() {
     rectangle(board, Rect(500, 200, 150, 100), Scalar(0, 0, 0), 2);     //Clear Rect(500, 200, 150, 100)
     rectangle(board, Rect(500, 300, 150, 100), Scalar(0, 0, 0), 2);     //Run   Rect(500, 300, 150, 100)
     rectangle(board, Rect(500, 400, 150, 100), Scalar(0, 0, 0), 2);     //Exit  Rect(500, 400, 150, 100)
-    putText(board, "Save", Point(535, 65), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Load", Point(525, 165), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Clear", Point(535, 265), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Run", Point(545, 365), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Exit", Point(545, 465), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
+    
+    copyimg(board, Rect(500, 0, 150, 100), save_black);
+    copyimg(board, Rect(500, 100, 150, 100), load_black);
+    copyimg(board, Rect(500, 200, 150, 100), clear_black);
+    copyimg(board, Rect(500, 300, 150, 100), run_black);
+    copyimg(board, Rect(500, 400, 150, 100), exit_black);
+    
     return board;
 }
-void wrigt_number(Mat board) {
-    board(Rect(501, 1, 148, 98)) = Scalar(255, 255, 255);
-    board(Rect(501, 101, 148, 98)) = Scalar(255, 255, 255);
-    board(Rect(501, 201, 148, 98)) = Scalar(255, 255, 255);
-    board(Rect(501, 301, 148, 98)) = Scalar(255, 255, 255);
-    board(Rect(501, 401, 148, 98)) = Scalar(255, 255, 255);
-    putText(board, "Save", Point(535, 65), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Load", Point(525, 165), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Clear", Point(535, 265), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Run", Point(545, 365), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
-    putText(board, "Exit", Point(545, 465), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
+void turn_menu_color(Mat board) {
+    copyimg(board, Rect(500, 0, 150, 100), save_black);
+    copyimg(board, Rect(500, 100, 150, 100), load_black);
+    copyimg(board, Rect(500, 200, 150, 100), clear_black);
+    copyimg(board, Rect(500, 300, 150, 100), run_black);
+    copyimg(board, Rect(500, 400, 150, 100), exit_black);
 }
 
 void on_mouse(int event, int x, int y, int flags, void* userdata) {
     static Point prePoint = Point(0, 0);
-    
-    if (Point(x, y).inside(Rect(500, 0, 148, 100))) {
-        wrigt_number(*(Mat*)userdata);
-        (*(Mat*)userdata)(Rect(501, 1, 148, 98)) = Scalar(255, 255, 255);
-        putText(*(Mat*)userdata, "Save", Point(535, 65), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 3);
+    Rect save(500, 0, 148, 100), load(500, 100, 148, 100), clear(500, 200, 148, 100), run(500, 300, 148, 100), exit_f(500, 400, 148, 100);
+    if (Point(x, y).inside(save)) {
+        turn_menu_color(*(Mat*)userdata);
+        copyimg(*(Mat*)userdata, save, save_red);
     }
-    else if (Point(x, y).inside(Rect(500, 100, 148, 100))) {
-        wrigt_number(*(Mat*)userdata);
-        (*(Mat*)userdata)(Rect(501, 101, 148, 98)) = Scalar(255, 255, 255);
-        putText(*(Mat*)userdata, "Load", Point(525, 165), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 3);
+    else if (Point(x, y).inside(load)) {
+        turn_menu_color(*(Mat*)userdata);
+        copyimg(*(Mat*)userdata, load, load_red);
     }
-    else if (Point(x, y).inside(Rect(500, 200, 148, 100))) {
-        wrigt_number(*(Mat*)userdata);
-        (*(Mat*)userdata)(Rect(501, 201, 148, 98)) = Scalar(255, 255, 255);
-        putText(*(Mat*)userdata, "Clear", Point(535, 265), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 3);
+    else if (Point(x, y).inside(clear)) {
+        turn_menu_color(*(Mat*)userdata);
+        copyimg(*(Mat*)userdata, clear, clear_red);
     }
-    else if (Point(x, y).inside(Rect(500, 300, 148, 100))) {
-        wrigt_number(*(Mat*)userdata);
-        (*(Mat*)userdata)(Rect(501, 301, 148, 98)) = Scalar(255, 255, 255);
-        putText(*(Mat*)userdata, "Run", Point(545, 365), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 3);
+    else if (Point(x, y).inside(run)) {
+        turn_menu_color(*(Mat*)userdata);
+        copyimg(*(Mat*)userdata, run, run_red);
     }
-    else if (Point(x, y).inside(Rect(500, 400, 148, 100))) {
-        wrigt_number(*(Mat*)userdata);
-        (*(Mat*)userdata)(Rect(501, 401, 148, 98)) = Scalar(255, 255, 255);
-        putText(*(Mat*)userdata, "Exit", Point(545, 465), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 3);
+    else if (Point(x, y).inside(exit_f)) {
+        turn_menu_color(*(Mat*)userdata);
+        copyimg(*(Mat*)userdata, exit_f, exit_red);
     }
     else {
-        wrigt_number(*(Mat*)userdata);
+        turn_menu_color(*(Mat*)userdata);
     }
 
     switch (event) {
